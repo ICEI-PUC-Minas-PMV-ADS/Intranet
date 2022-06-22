78c7356a4cc4426dc38dc3dba4bf978ae32551ac
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-06-22 14:01:31 -0300

    novasPaginas Paginas de Notícias

--- js/autoScroll.js
+++ js/autoScroll.js
@@ -1,10 +1,11 @@
-const servicesLink = document.getElementById('services')
-const servicesLinkMobile = document.getElementById('servicesMobile')
+const servicesLink = document.getElementById('services');
+const servicesLinkMobile = document.getElementById('servicesMobile');
 
 function scrollToSection(e) {
     e.preventDefault();
-    window.scrollTo(0, 1000)
+    window.scrollTo(0, 1000);
 }
 
-servicesLink.addEventListener('click', scrollToSection)
-servicesLinkMobile.addEventListener('click', scrollToSection)
+servicesLink.addEventListener('click', scrollToSection);
+servicesLinkMobile.addEventListener('click', scrollToSection);
+
